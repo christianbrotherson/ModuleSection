545b4ccc5c2f816982e652133f9438ca8cd42417
Christian Brotherson <christian.brotherson@gmail.com>
2018-03-15 16:09:25 -0600

    Practiced with importing and exporting multiple elements from different JavaScript files

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,1 +1,8 @@
-console.log('Hi there');
+import * as helper from './helper';
+import navigation from './navigation';
+
+console.log(navigation());
+
+console.log(helper.multiply(2, 18));
+console.log(helper.greeting);
+
